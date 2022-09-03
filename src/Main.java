032b2fd2d44b9a53ec504ed570f3e1f07b1c3fb6
inix84 <inix84@mail.ru>
2022-09-04 04:34:17 +0500

    дз 2, задание 2

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задание 1");
-        int age1 = 199;
-        if (age1 >= 18) {
+        int age = 28;
+        if (age >= 18) {
             System.out.println("Поздравляем, это совершеннолетие!");
         } else {
             System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать!");
+        }
+        System.out.println();
+        System.out.println("Задание 2");
+        if (age>=7 && age<18) {
+            System.out.println("ребенок ходит в школу!");
+        } else {
+            if (age >= 18 && age < 24) {
+                System.out.println("человек уже закончил школу и может отправляться в университет");
+            } else {
+                if (age >= 24) {
+                    System.out.println("человек окончил университет и ему пора искать первую работу");
+                }
+            }
         }
     }
 }
