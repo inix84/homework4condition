cbab58418de87bd46e5b677e72e74628221718c9
inix84 <inix84@mail.ru>
2022-09-04 04:52:19 +0500

    дз 3, задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        int age = 220;
+        if (age>=2 && age<=6) {
+            System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить в детский сад");
+        } else {
+            if (age>=7 && age<18) {
+                System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить в школу");
+            } else {if (age>=18 && age<24) {
+                System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить в университет");
+            }else {
+                if (age>24) {
+                    System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить на работу");
+                }
+            }
+            }
+        }
     }
 }
