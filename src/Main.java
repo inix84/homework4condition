44abdc98d9e15e49251a1c8d1afa1a1df97e6aad
inix84 <inix84@mail.ru>
2022-09-04 03:41:28 +0500

    дз 1, задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        int age=66;
+        if (age>=18) {
+            System.out.println("Поздравляем, вы уже совершеннолетний, и возможно уже давно!");
+        } else {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать!");
+        }
+        System.out.println();
+        System.out.println("Задание 2");
+
     }
 }
