81ed899dd200be7e8ffc8fbea89c951fb9854bdf
inix84 <inix84@mail.ru>
2022-09-04 05:04:20 +0500

    дз 3, задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        int age = 220;
+        int age = 15;
+
         if (age>=2 && age<=6) {
             System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить в детский сад");
         } else {
             if (age>=7 && age<18) {
                 System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить в школу");
-            } else {if (age>=18 && age<24) {
+            } else {
+                if (age>=18 && age<24) {
                 System.out.println("Если возраст человека равен "+ age + " , то ему нужно ходить в университет");
             }else {
                 if (age>24) {
@@ -16,5 +18,19 @@ public class Main {
             }
             }
         }
+        System.out.println();
+        System.out.println("Задание 2");
+
+        if (age<5) {
+            System.out.println("Если ребенку "+ age+ " года, то он не может кататься на аттракционе");
+        } else {
+            if (age>=5 && age<14) {
+                System.out.println("Если ребенку "+ age+ " лет, то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя");
+            } else {
+                    System.out.println("Если возраст человека равен "+ age + " , то он может кататься без сопровождения взрослого");
+                }
+            }
+
+        }
+
     }
-}
